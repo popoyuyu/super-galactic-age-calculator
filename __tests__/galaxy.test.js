b5182fac33098c76dc5b8190aa7b1f04b6948000
Jeff Lai <pjefflai@gmail.com>
2021-11-11 13:56:58 -0800

    calculate how many years to live on each planet 4th test

--- __tests__/galaxy.test.js
+++ __tests__/galaxy.test.js
@@ -6,17 +6,19 @@ describe("AgeCalc", () => {
     const age = new AgeCalc(3, "venus");
     expect(age.planetAge()).toEqual(1.86);
   });
+
   test("should calculate the age on planet", () => {
     const age = new AgeCalc(2, "mars");
     expect(age.planetAge()).toEqual(3.76);
   });
+
   test("should calculate the age on planet", () => {
     const age = new AgeCalc(43, "jupiter");
     expect(age.planetAge()).toEqual(509.98);
-  }); 
-  it works!
+  });
 
-
-
-
+  test("should calculate how many years left to live on each planet", () => {
+    const yrsLeft = new AgeCalc(3, "venus");
+    expect(yrsLeft.yearsToLive()).toEqual(70.14);
+  });
 });
