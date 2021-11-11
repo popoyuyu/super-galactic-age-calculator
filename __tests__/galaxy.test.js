58e8c1335e5d4d9aa6e4dab1dad4931443c691fb
Jeff Lai <pjefflai@gmail.com>
2021-11-11 11:21:45 -0800

    second test for age on Venus

--- __tests__/galaxy.test.js
+++ __tests__/galaxy.test.js
@@ -6,4 +6,11 @@ describe("AgeCalc", () => {
     const age = new AgeCalc(1);
     expect(age.mercuryAge()).toEqual(0.24);
   });
+
+  test("should calculate the age on Venus", () => {
+    const age = new AgeCalc(1);
+    exepct(age.mercuryAge()).toEqual(0.62);
+  });
+
+
 });
